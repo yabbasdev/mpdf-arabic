e8596a8d99d657372eeffbe51c5fc3c291c55e4d
yabbasdev <y.abbas@fulltek.co>
2025-06-29 16:36:30 +0300

    Arabic Support

--- src/Config/FontVariables.php
+++ src/Config/FontVariables.php
@@ -101,6 +101,15 @@ class FontVariables
 					'useOTL' => 0xFF,
 					'useKashida' => 75,
 				],
+				"jannalt" => [
+					'R' => "JannaLTRegular.ttf",
+					'B' => "JannaLTBold.ttf",
+					'I' => "JannaLTRegular.ttf",
+					'BI' => "JannaLTRegular.ttf",
+					'useOTL' => 0xFF,
+					'useKashida' => 75,
+				],
+
 				"dejavusans" => [
 					'R' => "DejaVuSans.ttf",
 					'B' => "DejaVuSans-Bold.ttf",
